1656f944a637389fa70f1005f67fce744432b274
Mati Coa <maticoarasa0@gmail.com>
2020-04-15 20:43:49 -0300

    inicializo log y config y creo bloques en tallgrass

---
+++ game_card/headers/filesystem.c
@@ -0,0 +1,27 @@
+#include "game_card.h"
+
+void inicializar_bloques(char* punto_montaje, int cantidad){
+
+	char* blocks_path = string_new();
+	string_append_with_format(&blocks_path, "%s/Blocks", punto_montaje);
+
+	FILE* block_file;
+
+	for(int i = 0; i < cantidad; i++){
+		char* block_path = string_new();
+		string_append_with_format(&block_path, "%s/%d.bin", blocks_path, i);
+
+		block_file = fopen(block_path, "wb");
+
+		if(block_file == NULL){
+			log_error(logger, "Error creando el bloque '%s'", block_path);
+			exit(1);
+		}
+
+		fclose(block_file);
+
+		free(block_path);
+	}
+
+	free(blocks_path);
+}
