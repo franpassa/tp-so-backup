2461d3e51874d0e524b24bb3b554c38fad82ea66
Tomas Martinez <martineztomashernan@gmail.com>
2020-04-20 17:22:09 -0300

    colas y subs

--- broker/broker.c
+++ broker/broker.c
@@ -3,7 +3,18 @@
 
 int main(){
 
-	printf("Hola");
-
+	/*t_cola_de_mensajes nuevo;
+	nuevo = inicializar_cola();
+	t_suscriptor uno;
+	uno.id=4;
+	uno.id_correlacional=2;
+	void* p =&uno;
+	int posicion = list_add(nuevo.lista_suscriptores, p);
+	void* p2 = list_get(nuevo.lista_suscriptores,posicion);
+	t_suscriptor sub = *(t_suscriptor*)p2;
+	printf("%d\n",sub.id);
+	printf("%d\n",sub.id_correlacional);
+	*/
 	return 0;
+
 }
--- broker/broker.h
+++ broker/broker.h
@@ -8,6 +8,43 @@
 #ifndef BROKER_H_
 #define BROKER_H_
 
+#include<commons/collections/queue.h>
+#include<commons/collections/list.h>
+#include <stdint.h>
+
+#define TRUE 0
+#define FALSE 1
+
+
+typedef struct{
+	uint32_t id;
+	//t_suscriptor enviados;
+	//tipo de mensaje
+	uint32_t ack;
+} t_mensaje;
+
+typedef struct{
+	uint32_t id;
+	uint32_t id_correlacional;
+} t_suscriptor;
+
+typedef struct{
+	t_queue* cola;
+	t_list* lista_suscriptores;
+
+}t_cola_de_mensajes;
+
+
+t_cola_de_mensajes inicializar_cola(){
+	t_cola_de_mensajes nueva_cola_de_mensajes;
+	t_queue* nueva_cola = queue_create();
+	nueva_cola_de_mensajes.cola = nueva_cola;
+	nueva_cola_de_mensajes.lista_suscriptores = list_create();
+	return nueva_cola_de_mensajes;
+}
+
+
+
 
 //inicializar_colas()
 //recibir mensaje() -- hilo del broker
