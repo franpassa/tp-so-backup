c72dfc748f5e5d2028a05d40281603a3b75db6e9
franpassa <francopassarelli94@gmail.com>
2020-04-28 16:33:14 -0300

    cambio a crearListaDeEntrenadores

--- broker/broker.h
+++ broker/broker.h
@@ -8,17 +8,15 @@
 #ifndef BROKER_H_
 #define BROKER_H_
 
+#include <stdio.h>
 #include <commons/collections/queue.h>
 #include <commons/collections/list.h>
+#include <commons/log.h>
 #include <stdint.h>
 #include <pthread.h>
+#include<commons/config.h>
+#include<sys/socket.h>
 
-t_cola_de_mensajes NEW_POKEMON;
-t_cola_de_mensajes APPEARED_POKEMON;
-t_cola_de_mensajes CATCH_POKEMON;
-t_cola_de_mensajes CAUGHT_POKEMON;
-t_cola_de_mensajes GET_POKEMON;
-t_cola_de_mensajes LOCALIZED_POKEMON;
 
 typedef struct{
 	uint32_t id;
@@ -36,6 +34,20 @@ typedef struct{
 	t_list* lista_suscriptores;
 }t_cola_de_mensajes;
 
+t_cola_de_mensajes NEW_POKEMON;
+t_cola_de_mensajes APPEARED_POKEMON;
+t_cola_de_mensajes CATCH_POKEMON;
+t_cola_de_mensajes CAUGHT_POKEMON;
+t_cola_de_mensajes GET_POKEMON;
+t_cola_de_mensajes LOCALIZED_POKEMON;
+
+t_log* logger;
+t_config* config;
+
+t_log* iniciar_logger(void);
+t_config* leer_config(void);
+void terminar_programa(t_log*, t_config*);
+
 
 t_cola_de_mensajes inicializar_cola(t_cola_de_mensajes nombre_cola){
 
@@ -45,24 +57,34 @@ t_cola_de_mensajes inicializar_cola(t_cola_de_mensajes nombre_cola){
 	return nombre_cola;
 }
 
-void recibir_mensajes(){
+/*void recibir_mensajes(){
+	int id_cola,id_tipo;
 
 	while(1){
-	int id_cola = //sacar info de mensaje
-	char* mensaje = //sacar info de mensaje
-	int id_mensaje = crear_id_mensaje();
 
-	enviar_a_publisher_id(id_mensaje);
+	id_tipo = // sacar info de mensaje
+	id_cola = //sacar info de mensaje
 
-	t_info_mensaje nuevo_mensaje;
-	nuevo_mensaje.id = id_mensaje;
-	nuevo_mensaje.mensaje = mensaje;
-	nuevo_mensaje.a_quienes_fue_enviado = NULL;
-	nuevo_mensaje.cuantos_lo_recibieron = NULL;
+	if (si tipo es original){
+		char* mensaje = //sacar info de mensaje
+			if (mensaje no esta en cola/){
+				int id_mensaje = crear_id_mensaje();
 
-	agregar_a_cola(id_cola,nuevo_mensaje);
+				enviar_a_publisher_id(id_mensaje);
+
+				t_info_mensaje nuevo_mensaje;
+				nuevo_mensaje.id = id_mensaje;
+				nuevo_mensaje.mensaje = mensaje;
+				nuevo_mensaje.a_quienes_fue_enviado = NULL;
+				nuevo_mensaje.cuantos_lo_recibieron = NULL;
+
+				agregar_a_cola(id_cola,nuevo_mensaje);
+				}
+	} else {
+		int id_correlativo = // sacar info de mensaje
+		confirmar_mensaje(id_cola , id_correlativo);
 	}
-}
+}*/
 
 void enviar_a_publisher_id(int id){
 
@@ -78,7 +100,6 @@ void enviar_a_publisher_id(int id){
 //MEMORIA
 //dump_cache()
 //compactar_memoria()
-
 
 
 
