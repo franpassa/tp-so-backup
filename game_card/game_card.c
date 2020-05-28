1b3ca652b2aaba0e275a86ecc15343365b86d1b3
Tomas Martinez <martineztomashernan@gmail.com>
2020-05-28 16:17:19 -0300

    arreglo de hilo enviar mensaje

--- game_card/game_card.c
+++ game_card/game_card.c
@@ -14,23 +14,30 @@ void terminar(){
 	config_destroy(config);
 }
 
+
 int main(){
 
-	//inicializar();
+	/*int socket_broker = suscribirse_a_cola(NEW_POKEMON, "127.0.0.1", "6009");
+	int socket_broker2 = suscribirse_a_cola(NEW_POKEMON, "127.0.0.1", "6009");
 
-	//int socket_brok = conectar_a_broker(CATCH_POKEMON, "127.0.0.1", "6009");
-	//while(1){
-	//new_pokemon_msg* msg = new_msg("Hola soy Pikachu", 3, 4, 10);
-	//uint32_t id = enviar_mensaje("127.0.0.1","6009",NEW_POKEMON, (void*) msg,true);
-	//printf("id recibido: %d\n", id);
-	//}
-	/*t_bitarray* bitarray = read_bitmap(fspaths->bitmap_file);
-	print_bitarray(bitarray);
-	set_bit(fspaths->bitmap_file, 2, true);
-	t_bitarray* bitarray2 = read_bitmap(fspaths->bitmap_file);
-	print_bitarray(bitarray2);*/
+	new_pokemon_msg* msg = new_msg("Hola soy Pikachu", 3, 4, 10);
+	uint32_t id = enviar_mensaje("127.0.0.1","6009",NEW_POKEMON, (void*) msg,true);
+	printf("id recibido: %d\n", id);
 
-	//terminar();
+	uint32_t id_recibido;
+	new_pokemon_msg* msg_recibido = (new_pokemon_msg*) recibir_mensaje(NEW_POKEMON, socket_broker, &id_recibido);
+
+	new_pokemon_msg* msg2 = new_msg("Hola soy Squirtle", 3, 4, 10);
+	uint32_t id2 = enviar_mensaje("127.0.0.1","6009",NEW_POKEMON, (void*) msg2,true);
+	printf("id recibido: %d\n", id2);
+
+	uint32_t id_recibido2;
+	new_pokemon_msg* msg_recibido2 = (new_pokemon_msg*) recibir_mensaje(NEW_POKEMON, socket_broker2, &id_recibido2);
+
+	print_msg(NEW_POKEMON, (void*) msg_recibido);
+	print_msg(NEW_POKEMON, (void*) msg_recibido2);
+
+	while(1);*/
 
 	return EXIT_SUCCESS;
 }
