5fd651b38d0032a3a7662a96c97d9281e3ecadae
Tomas Martinez <martineztomashernan@gmail.com>
2020-05-30 17:17:30 -0300

    corrigo inicializacion de semaforo y dejo limpio el main de gamecard

--- game_card/game_card.c
+++ game_card/game_card.c
@@ -17,30 +17,5 @@ void terminar(){
 
 int main(){
 
-
-	int socket_broker = suscribirse_a_cola(NEW_POKEMON, "127.0.0.1", "6009");
-	int socket_broker2 = suscribirse_a_cola(NEW_POKEMON, "127.0.0.1", "6009");
-
-	new_pokemon_msg* msg = new_msg("Hola soy Pikachu", 3, 4, 10);
-	uint32_t id = enviar_mensaje("127.0.0.1","6009",NEW_POKEMON, (void*) msg,true);
-	printf("id recibido: %d\n", id);
-
-	uint32_t id_recibido;
-	new_pokemon_msg* msg_recibido = (new_pokemon_msg*) recibir_mensaje(NEW_POKEMON,&id_recibido);
-
-	new_pokemon_msg* msg2 = new_msg("Hola soy Squirtle", 3, 4, 10);
-	uint32_t id2 = enviar_mensaje("127.0.0.1","6009",NEW_POKEMON, (void*) msg2,true);
-	printf("id recibido: %d\n", id2);
-
-	uint32_t id_recibido2;
-	new_pokemon_msg* msg_recibido2 = (new_pokemon_msg*) recibir_mensaje(NEW_POKEMON,&id_recibido2);
-
-	print_msg(NEW_POKEMON, (void*) msg_recibido);
-	print_msg(NEW_POKEMON, (void*) msg_recibido2);
-
-
-	while(1);
-
-
 	return EXIT_SUCCESS;
 }
