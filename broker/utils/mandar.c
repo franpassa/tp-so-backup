3f27602c9fceccde8cf7ecbd6ad4319c33894212
Tomas Martinez <martineztomashernan@gmail.com>
2020-05-31 02:29:30 -0300

    arreglo error de sincro

--- broker/utils/mandar.c
+++ broker/utils/mandar.c
@@ -9,7 +9,7 @@ void mandar_mensajes() {
 
 			cola_actual = NEW_POKEMON;
 		}
-		if (sem_trywait(&contenido_cola[cola_actual]) == 0) {
+		if (queue_size(int_a_nombre_cola(cola_actual)->cola) != 0) { // sem_trywait(&contenido_cola[cola_actual]) == 0
 			pthread_mutex_lock(&(sem_cola[cola_actual]));
 			recorrer_cola(int_a_nombre_cola(cola_actual));
 			pthread_mutex_unlock(&(sem_cola[cola_actual]));
