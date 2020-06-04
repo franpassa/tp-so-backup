cb41eaa2fad7b7369859ceca2b89979529162410
lopez-juli <julianlopez201313@gmail.com>
2020-06-04 20:22:00 -0300

    que hacer si sub se desconecta de cola broker

--- broker/utils/mandar.c
+++ broker/utils/mandar.c
@@ -18,15 +18,15 @@ void mandar_mensajes() {
 	}
 }
 
-void mandar(t_paquete* paquete, uint32_t id, uint32_t sub) {
+int mandar(t_paquete* paquete, uint32_t id, uint32_t sub) {
 	int total_bytes = paquete->buffer->size + sizeof(queue_name) + sizeof(uint32_t);
 	void* bytes = serializar_paquete(paquete, total_bytes);
 	if ( send(sub, bytes, total_bytes, 0) == -1){
-		//sacar de listas
-
-	} else {
-	send(sub, &id, sizeof(uint32_t), 0);
-	}
+		return 1;
+		} else {
+			send(sub, &id, sizeof(uint32_t), 0);
+			return 0;
+		}
 
 }
 
@@ -38,6 +38,8 @@ void recorrer_cola(t_cola_de_mensajes* nombre) {
 			uint32_t id_primero = info_a_sacar->id;
 			uint32_t id_siguiente;
 
+			bool sub_subscrito = true;
+
 			do {
 				info_a_sacar = queue_pop(nombre->cola);
 
@@ -45,8 +47,27 @@ void recorrer_cola(t_cola_de_mensajes* nombre) {
 				for (int i = 0; i < list_size(nombre->lista_suscriptores); i++) {
 					uint32_t* sub = (uint32_t*) list_get(nombre->lista_suscriptores, i);
 					if (!esta_en_lista(info_a_sacar->quienes_lo_recibieron,sub)) {
-						mandar(info_a_sacar->paquete, info_a_sacar->id, *sub);
-						if (!esta_en_lista(info_a_sacar->a_quienes_fue_enviado,sub)){
+						if (mandar(info_a_sacar->paquete, info_a_sacar->id, *sub)){
+							sub_subscrito = false;
+							bool es_igual_a(void* uno) {
+								uint32_t nro = *(uint32_t*) uno;
+								return nro == *sub;
+							}
+							list_remove_by_condition(nombre->lista_suscriptores,es_igual_a);
+							uint32_t id_afuera = info_a_sacar->id;
+							do {
+								list_remove_by_condition(info_a_sacar->a_quienes_fue_enviado,es_igual_a);
+								list_remove_by_condition(info_a_sacar->quienes_lo_recibieron,es_igual_a);
+
+
+								queue_push(nombre->cola, info_a_sacar);
+								info_a_sacar = queue_pop(nombre->cola);
+								id_siguiente = info_a_sacar->id;
+
+							} while (id_afuera != id_siguiente);
+
+						}
+						if (!esta_en_lista(info_a_sacar->a_quienes_fue_enviado,sub) && sub_subscrito){
 							list_add(info_a_sacar->a_quienes_fue_enviado, sub);
 						}
 					}
