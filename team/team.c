aa49e48a025c379c70134197d7033584f4af987c
franpassa <francopassarelli94@gmail.com>
2020-06-07 16:50:03 -0300

    Ultimos cambios

--- team/team.c
+++ team/team.c
@@ -5,47 +5,47 @@ int main()
 	inicializarPrograma(); //Inicializo logger y config
 	inicializarVariables();
 
-//	pthread_t hilo_escucha;
+	pthread_t hilo_escucha;
 	pthread_t hilo_estado_exec;
 	pthread_t hilo_pasar_a_ready;
 //	pthread_t hilo_recibir_localized;
 //	pthread_t hilo_recibir_caught;
 //	pthread_t hilo_recibir_appeared;
 
-//	printf("\n");
-//	int socket_escucha = iniciar_servidor(IP,PUERTO);
-//	printf("\n");
-//	if (socket_escucha == -1) abort(); //FINALIZA EL PROGRAMA EN CASO DE QUE FALLE LA INICIALIZACION DEL SERVIDOR
+	printf("\n");
+	int socket_escucha = iniciar_servidor(IP,PUERTO);
+	printf("\n");
+	if (socket_escucha == -1) abort(); //FINALIZA EL PROGRAMA EN CASO DE QUE FALLE LA INICIALIZACION DEL SERVIDOR
 
-	t_pokemon* pikachu = malloc(sizeof(t_pokemon));
-	pikachu->nombre = "penitis";
-	pikachu->posicionX = 10;
-	pikachu->posicionY = 17;
-
-	t_pokemon* squirtle = malloc(sizeof(t_pokemon));
-	squirtle->nombre = "EL QUE LA TIENE MAS LARGONATOR";
-	squirtle->posicionX = 1000;
-	squirtle->posicionY = 200;
-
-	list_add(pokemons_recibidos,pikachu);
-
-	list_add(pokemons_recibidos,squirtle);
+//	t_pokemon* pikachu = malloc(sizeof(t_pokemon));
+//	pikachu->nombre = "penitis";
+//	pikachu->posicionX = 10;
+//	pikachu->posicionY = 17;
+//
+//	t_pokemon* squirtle = malloc(sizeof(t_pokemon));
+//	squirtle->nombre = "EL QUE LA TIENE MAS LARGONATOR";
+//	squirtle->posicionX = 14;
+//	squirtle->posicionY = 19;
+//
+//	list_add(pokemons_recibidos,pikachu);
+//
+//	list_add(pokemons_recibidos,squirtle);
 
 //	enviar_gets(objetivos_globales); // ENVIO MENSAJES GET_POKEMON AL BROKER.
 
 	pthread_create(&hilo_pasar_a_ready,NULL,(void*) pasar_a_ready, NULL);
 
-//	pthread_create(&hilo_escucha,NULL,(void*) esperar_cliente, &socket_escucha);
-//
+	pthread_create(&hilo_escucha,NULL,(void*) esperar_cliente, &socket_escucha);
+
 	pthread_create(&hilo_estado_exec, NULL, (void*) estado_exec, NULL);
-//
+
 //	pthread_create(&hilo_recibir_localized, NULL, (void*) recibirLocalized, NULL);
 //
 //	pthread_create(&hilo_recibir_caught, NULL, (void*) recibirCaught, NULL);
 //
 //	pthread_create(&hilo_recibir_appeared, NULL, (void*) recibirAppeared, NULL);
 
-//	pthread_join(hilo_escucha, NULL);
+	pthread_join(hilo_escucha, NULL);
 	pthread_join(hilo_estado_exec, NULL);
 	pthread_join(hilo_pasar_a_ready, NULL);
 //	pthread_join(hilo_recibir_localized, NULL);
