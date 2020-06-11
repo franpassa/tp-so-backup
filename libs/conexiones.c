1f85f681024ed71dc01e66a6b3f0ad17b924ba5c
Mati Coa <maticoarasa0@gmail.com>
2020-06-11 19:46:39 -0300

    agrego opcion para mandar id de mensaje en enviar_mensaje y recibir tipo de mensaje en recibir_mensaje

--- libs/conexiones.c
+++ libs/conexiones.c
@@ -97,10 +97,10 @@ int suscribirse_a_cola(queue_name cola, char* ip, char* puerto) {
 	}
 }
 
-uint32_t enviar_mensaje(char* ip, char* puerto, queue_name cola, void* estructura_mensaje, bool esperar_id) {
+uint32_t enviar_mensaje(char* ip, char* puerto, queue_name cola, void* estructura_mensaje, uint32_t id_mensaje, bool esperar_id_respuesta) {
 
 	int socket_receptor = conectar_como_productor(ip, puerto);
-	if (socket_receptor == -1) return -1;
+	if (socket_receptor == -1) return -1; // Es un uint32_t y estoy devolviendo -1...
 
 	t_paquete* paquete = malloc(sizeof(t_paquete));
 
@@ -224,13 +224,17 @@ uint32_t enviar_mensaje(char* ip, char* puerto, queue_name cola, void* estructur
 		free_paquete(paquete);
 		free(a_enviar);
 		return -1;
+	} else {
+		if(id_mensaje > 0){
+			send(socket_receptor, &id_mensaje, sizeof(uint32_t), 0);
+		}
 	}
 
 	free_paquete(paquete);
 	free(a_enviar);
 
 	uint32_t id;
-	if (esperar_id) {
+	if (esperar_id_respuesta) {
 		int status_recv = recv(socket_receptor, &id, sizeof(uint32_t), MSG_WAITALL);
 		if (status_recv <= 0) id = -1;
 	} else {
@@ -274,7 +278,7 @@ void confirmar_recepcion(queue_name cola, uint32_t id_mensaje, uint32_t socket_s
 	send(socket_broker, a_enviar, bytes, 0);
 }
 
-void* recibir_mensaje(int socket, uint32_t* id) {
+void* recibir_mensaje(int socket, uint32_t* id, queue_name* tipo_msg) {
 
 	t_paquete* paquete = malloc(sizeof(t_paquete));
 
@@ -282,6 +286,7 @@ void* recibir_mensaje(int socket, uint32_t* id) {
 		free(paquete);
 		return NULL;
 	}
+	*tipo_msg = paquete->cola_msg;
 
 	paquete->buffer = malloc(sizeof(t_buffer));
 	if (recv(socket, &(paquete->buffer->size), sizeof(uint32_t), MSG_WAITALL) <= 0) {
@@ -299,7 +304,7 @@ void* recibir_mensaje(int socket, uint32_t* id) {
 		*id = 0; // No se envio o hubo un error recibiendo el ID.
 	}
 
-	void* msg = deserializar_buffer(paquete->cola_msg, paquete->buffer);
+	void* msg = deserializar_buffer(*tipo_msg, paquete->buffer);
 	free_paquete(paquete);
 
 	return msg;
--- libs/conexiones.h
+++ libs/conexiones.h
@@ -94,8 +94,9 @@ int iniciar_servidor(char* ip, char* puerto);
 // Pasa 'queue_name' = PRODUCTOR y no espera confirmación del Broker.
 int conectar_como_productor(char* ip, char* puerto);
 int suscribirse_a_cola(queue_name cola, char* ip, char* puerto);
-uint32_t enviar_mensaje(char* ip, char* puerto, queue_name cola, void* estructura_mensaje, bool esperar_id);
-void* recibir_mensaje(int socket, uint32_t* id_mensaje);
+// Si no se le quiere pasar el ID del mensaje, se le pasa 0.
+uint32_t enviar_mensaje(char* ip, char* puerto, queue_name cola, void* estructura_mensaje, uint32_t id_mensaje, bool esperar_id_rta);
+void* recibir_mensaje(int socket, uint32_t* id_mensaje, queue_name* tipo_msg);
 void confirmar_recepcion(queue_name cola, uint32_t id_mensaje, uint32_t socket_suscripcion, uint32_t socket_broker); // SE ENVÍA queue_name - size - id_mensaje - socket_suscripcion
 void* serializar_paquete(t_paquete* paquete, int bytes);
 void* deserializar_buffer(queue_name cola, void* buffer_ptr);
