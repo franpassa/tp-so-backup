cf04294fc5054e0132ad48cd41d48ee089b35272
franpassa <francopassarelli94@gmail.com>
2020-07-21 16:32:38 -0300

    Arreglo errores que saltaban con valgrind, agrego mutex faltantes, y agrego un bool a la estructura del entrenador para el SJF

--- team/team.c
+++ team/team.c
@@ -128,9 +128,9 @@ void liberarVariables()
 	liberarArray(pokesObjetivos);
 	list_destroy_and_destroy_elements(pokemons_objetivos, free);
 	list_destroy_and_destroy_elements(objetivos_globales,liberarEspecie);
-	list_destroy(estado_bloqueado);
-	list_destroy(estado_new);
-	list_destroy(estado_exit);
+	list_destroy_and_destroy_elements(estado_bloqueado,liberarEntrenador);
+	list_destroy_and_destroy_elements(estado_new,liberarEntrenador);
+	list_destroy_and_destroy_elements(estado_exit,liberarEntrenador);
 	list_destroy_and_destroy_elements(pokemons_recibidos_historicos,liberarPokemon);
 	list_destroy_and_destroy_elements(pokemons_recibidos,liberarPokemon);
 	list_destroy_and_destroy_elements(ids_enviados,free);
